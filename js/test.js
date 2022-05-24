7410d940da89552d162939bebf43929fb3fdb655
Kinto-android-dev-cho <84059790+Kinto-android-dev-cho@users.noreply.github.com>
2022-05-25 08:23:18 +0900

    c

--- js/test.js
+++ js/test.js
@@ -9,24 +9,24 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
+    setTimeout(function(){
+        var userAgent = navigator.userAgent; 
+        var visitTime = (new Date()).getTime(); 
+        if (userAgent.match(/Android/i)) { //Android
+            location.href = 'http://play.google.com/store/apps/details?id=jp.co.toyota.myroute'
+        } else if (userAgent.match(/iPhone/i)) { //iOs
+         setTimeout(function() { 
+             if ((new Date()).getTime() - visitTime < 3000) { 
+                 location.href = "https://myroute.onelink.me/reBz/hrm4p32z"; 
+                 }
+             } ,2500);
+              setTimeout(function() { 
+                  location.href = "https://"; 
+                  } ,0); 
+        } else { //Window PC
+            location.href = 'http://play.google.com/store/apps/details?id=jp.co.toyota.myroute'
+        } 
 
-    var userAgent = navigator.userAgent; 
-    var visitTime = (new Date()).getTime(); 
-    if (userAgent.match(/Android/i)) { //Android
-        location.href = 'http://play.google.com/store/apps/details?id=jp.co.toyota.myroute'
-    } else if (userAgent.match(/iPhone/i)) { //iOs
-     setTimeout(function() { 
-         if ((new Date()).getTime() - visitTime < 3000) { 
-             location.href = "https://myroute.onelink.me/reBz/hrm4p32z"; 
-             }
-         } ,2500);
-          setTimeout(function() { 
-              location.href = "https://"; 
-              } ,0); 
-    } else { //Window PC
-        location.href = 'http://play.google.com/store/apps/details?id=com.google.android.apps.maps'
-    } 
-
-    // location.href = "https://yahoo.co.jp"
+    }, 2000);
 
 });
